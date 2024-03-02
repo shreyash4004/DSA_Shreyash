1cefbf325527a13e67671fcb3a146d35bfc40f51
Shreyash Bandi <shreyashbandi19@gmail.com>
2024-03-02 19:10:15 +0530

    Fourth Commit

--- Recursion/PrintNumbersIncreasing.java
+++ Recursion/PrintNumbersIncreasing.java
@@ -1,5 +1,16 @@
 package Recursion;
-
 public class PrintNumbersIncreasing {
 
+    public static void main(String[] args) {
+        int no=5;
+        Print(no);
+    }
+    public  static void Print(int no){
+        if(no==1){
+            System.out.println(no);
+            return;
+        }
+        Print(no-1);
+        System.out.println(no);
+    }
 }
