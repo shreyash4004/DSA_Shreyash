71ac3e7b1318b4ff22fa9afb00941a93ce057227
Shreyash Bandi <shreyashbandi19@gmail.com>
2024-03-02 22:18:58 +0530

    Sum Numbers

---
+++ Recursion/SumOfNumbers.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int no=5;
+        sum(no);
+        int ans=sum(no);
+        System.out.println(ans);
+    }
+    public static int  sum(int no){
+
+        if(no==1){
+            return 1;
+        }
+        int call=sum(no-1); 
+        int sum=no+call;
+        return sum;
+    }
+}
