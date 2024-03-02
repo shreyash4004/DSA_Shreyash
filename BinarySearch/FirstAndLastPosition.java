47e84087d0dc83394c90923883da0297687173c4
Shreyash Bandi <shreyashbandi19@gmail.com>
2024-03-02 17:22:49 +0530

    Third Commit

---
+++ BinarySearch/FirstAndLastPosition.java
@@ -0,0 +1,44 @@
+package BinarySearch;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+        int[] arr={5,7,7,7,7,8,8,10};
+        int target=7;
+        int[] ans=new int[2];
+        ans=searchRange(arr,target);
+    }
+    public static int[] searchRange(int[] nums, int target) {
+        int ans[]={-1,-1};
+        int start=search(nums,target,true);
+        int end=search(nums,target,false);
+        ans[0]=start;
+        ans[1]=end;
+
+        return ans;
+    }
+    public  static int search(int[] nums,int target,boolean b){
+        int ans=-1;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+
+
+            if(nums[mid]<target){
+               start=mid+1;
+            } else if(nums[mid]>target) {
+                end=mid-1;
+            }
+            else{
+                ans=mid;
+                if(b){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+}
