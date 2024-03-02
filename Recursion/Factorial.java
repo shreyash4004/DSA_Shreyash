5c4b2c7626c84446d30706343b9e55ff6be470d0
Shreyash Bandi <shreyashbandi19@gmail.com>
2024-03-02 19:38:46 +0530

    Factorial

---
+++ Recursion/Factorial.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.sql.SQLOutput;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int no=5;
+        int ans=fact(no);
+        System.out.println(ans);
+    }
+
+
+
+    public  static  int fact(int no){
+        if(no==0){
+            return 1;
+        }
+
+        fact(no-1);
+        int fact=no*fact(no-1);
+        return fact;
+    }
+}
--- Recursion/PrintNumbersIncreasing.java
+++ Recursion/PrintNumbersIncreasing.java
@@ -14,3 +14,4 @@ public class PrintNumbersIncreasing {
         System.out.println(no);
     }
 }
+
